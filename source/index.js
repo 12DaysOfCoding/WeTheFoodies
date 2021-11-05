15896a51a81be7d2cf78a7febd86017ca5e01f61
Lexseal Lin <zhongjiezhex12@me.com>
2021-11-04 20:59:47 -0700

    revert lint tests

--- source/index.js
+++ source/index.js
@@ -1,17 +1,3 @@
 /**
  * Entry point
  */
-
-// currently this contains some simple test code to verify the linter
-let test = () => {
-  let arr = [];
-  for (let i = 0; i < 10; i++) {
-    let greeting = 'hello ';
-    greeting += parseInt(i);
-    arr.push(greeting);
-  }
-
-  arr.forEach(elem => console.log(elem));
-};
-
-test();
