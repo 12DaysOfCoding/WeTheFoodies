95108b55958ccdf46b53748d11d2f01f959afb9e
Bin <binnybn98@gmail.com>
2021-11-15 22:43:15 -0800

    nav bar complete

--- source/assets/scripts/index.js
+++ source/assets/scripts/index.js
@@ -1,8 +1,13 @@
 // index.js
-  
+
 window.addEventListener('DOMContentLoaded', init);
-  
+
 async function init() {
+  goDashboard();
+  goSearch();
+  goAdd();
+  goSettings();
+
 
   // Create a recipe card with mock data
   let recipeCard = document.createElement('recipe-card');
@@ -10,4 +15,33 @@ async function init() {
   document.querySelector('.saved-recipes__wrapper').appendChild(recipeCard);
 
 }
-  
+
+function goDashboard() {
+  const btn = document.getElementsByClassName('nav-dashboard');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("index.html");
+  });
+}
+
+function goSearch() {
+  const btn = document.getElementsByClassName('nav-search');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("recipe-searchPage.html");
+  });
+}
+function goAdd() {
+  const btn = document.getElementsByClassName('nav-add');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("#");
+  });
+}
+function goSettings() {
+  const btn = document.getElementsByClassName('nav-settings');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("#");
+  });
+}
--- source/assets/scripts/recipe-searchPage.js
+++ source/assets/scripts/recipe-searchPage.js
@@ -1,14 +1,18 @@
 // recipe-searchPage.js
 
 window.addEventListener('DOMContentLoaded', init);
-  
+
 async function init() {
+  goDashboard();
+  goSearch();
+  goAdd();
+  goSettings();
 
   // Create a recipe card with mock data
   let recipeCard = document.createElement('recipe-card');
   recipeCard.data = {};
   document.querySelector('.recipes__wrapper').appendChild(recipeCard);
-  
+
   let recipeCard2 = document.createElement('recipe-card');
   recipeCard2.data = {};
   document.querySelector('.recipes__wrapper').appendChild(recipeCard2);
@@ -22,4 +26,33 @@ async function init() {
   // document.querySelector('.recipes__wrappers').appendChild(recipeCard4);
 
 }
-  
+
+function goDashboard() {
+  const btn = document.getElementsByClassName('nav-dashboard');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("index.html");
+  });
+}
+
+function goSearch() {
+  const btn = document.getElementsByClassName('nav-search');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("recipe-searchPage.html");
+  });
+}
+function goAdd() {
+  const btn = document.getElementsByClassName('nav-add');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("#");
+  });
+}
+function goSettings() {
+  const btn = document.getElementsByClassName('nav-settings');
+
+  btn[0].addEventListener('click', () => {
+    window.location.replace("#");
+  });
+}
