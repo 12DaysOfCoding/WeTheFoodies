9eb38a12a345a61c46f4976a3007859e635f2dba
Lexseal Lin <zhongjiezhex12@me.com>
2021-11-15 20:52:25 -0800

    added end to end test

--- tests/unit/example.test.js
+++ tests/unit/example.test.js
@@ -2,11 +2,7 @@
  * example for how to write a test case as well as to test if the testing framework works
  */
 
-// import the testing framework since we are using ES6 Modules
-import { jest, expect, test } from '@jest/globals';
-jest.useFakeTimers();
-
-import { add } from '../source/example.js';
+import { add } from '../../source/example.js';
 
 test('testing the add function in order to test the testing framework : )', () => {
   expect(add(1, 2)).toBe(3);
