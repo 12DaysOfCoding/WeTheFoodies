1c240f3b622353c72132c82fe18d8eac481fb27d
Yizhou Wang <81718264+yiw008@users.noreply.github.com>
2021-11-24 14:27:14 -0800

    Update api.js

--- source/assets/scripts/api.js
+++ source/assets/scripts/api.js
@@ -166,7 +166,7 @@ function remove_custom(id){
 
 /**
  * This function will get a array of intolerance then store them into local storage with the key intolerance
- * @param {array of strin } intolerance_list  a list of intolerance that user applied
+ * @param {array} intolerance_list - a list of intolerance that user applied
  */
  function set_intolerance(intolerance_list){
   let intolerance=get_info_localstore('intolerance');
