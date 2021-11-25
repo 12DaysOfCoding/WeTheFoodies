76cc784c1a4682160f1a8ad1965859ef8b2f25ad
sora1998 <38894303+sora1998@users.noreply.github.com>
2021-11-24 20:12:39 -0800

    Update api.js

--- source/assets/scripts/api.js
+++ source/assets/scripts/api.js
@@ -44,9 +44,9 @@ export async function search_recipe(name, intolerances) {
 // Parameter:
 // recipe_name: the recipe name 
 // it will automaticlt get and parse the recipe in local storage if it can't find it it will just print a sentence to console,.
-export function get_info_localstore(recipe_name){
-  if(localStorage.getItem(recipe_name)) {
-    return JSON.parse(localStorage.getItem(recipe_name));
+export function get_info_localstore(recipe_id){
+  if(localStorage.getItem(recipe_id)) {
+    return JSON.parse(localStorage.getItem(recipe_id));
   } else {
     console.log('Can not find recipe info in local,can not get');
     return undefined;
@@ -58,20 +58,16 @@ export function get_info_localstore(recipe_name){
 // recipe_info: the recipe object
 // it will automaticlt change the info to string and store it in the local storage, 
 // if locale storage already have it it will just print a sentence to console.
-export function set_localstore(recipe_name,recipe_info){
-  if(!localStorage.getItem(recipe_name)) {
-    localStorage.setItem(recipe_name,JSON.stringify(recipe_info));
-  } else {
-    console.log('Already Saved this recipe before,can not save it again');
-  }
+export function set_localstore(recipe_id,recipe_info){
+    localStorage.setItem(recipe_id,JSON.stringify(recipe_info));
 }
 
 // Parameter:
 // recipe_name: the recipe name 
 // it will automaticlt remove the recipe in local storage if it can't find it it will just print a sentence to console,.
-export function remove_localstore(recipe_name){
-  if(localStorage.getItem(recipe_name)) {
-    localStorage.removeItem(recipe_name);
+export function remove_localstore(recipe_id){
+  if(localStorage.getItem(recipe_id)) {
+    localStorage.removeItem(recipe_id);
   } else {
     console.log('Do not have recipe info in local, can not remove');
   }
@@ -170,12 +166,5 @@ export function remove_custom(id){
  * @param {Array<string>} intolerance_list - a list of intolerance that user applied
  */
  export function set_intolerance(intolerance_list){
-  let intolerance=get_info_localstore('intolerance');
-  if(intolerance==undefined){
     set_localstore('intolerance',intolerance_list);
-  }
-  else{
-    remove_localstore('intolerance');
-    set_localstore('intolerance',intolerance_list);
-  }
 }
