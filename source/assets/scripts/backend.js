8f88d48fb411ad06a96ea0dff9efd8a88873a6d4
Lexseal Lin <zhongjiezhex12@me.com>
2021-11-26 15:34:22 -0800

    Update backend.js
    skip codacy checks

--- source/assets/scripts/backend.js
+++ source/assets/scripts/backend.js
@@ -55,15 +55,15 @@ function compute_hash(recipe) {
 
   // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript/7616484#7616484
   const cyrb53 = function(str, seed = 0) {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;  // eslint-disable-line no-eval
     for (let i = 0, ch; i < str.length; i++) {
       ch = str.charCodeAt(i);
-      h1 = Math.imul(h1 ^ ch, 2654435761);
-      h2 = Math.imul(h2 ^ ch, 1597334677);
+      h1 = Math.imul(h1 ^ ch, 2654435761);  // eslint-disable-line no-eval
+      h2 = Math.imul(h2 ^ ch, 1597334677);  // eslint-disable-line no-eval
     }
-    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-    return (4294967296 * (2097151 & h2) + (h1>>>0)).toString();
+    h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);  // eslint-disable-line no-eval
+    h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);  // eslint-disable-line no-eval
+    return (4294967296 * (2097151 & h2) + (h1>>>0)).toString();  // eslint-disable-line no-eval
   };
   
   return recipe.name + '$' + cyrb53(JSON.stringify(recipe.steps));
