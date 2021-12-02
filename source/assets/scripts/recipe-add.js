99350ab22f9c4f65f2cb434d2e0b47200be1632f
prorick <56745348+prorick@users.noreply.github.com>
2021-12-02 00:10:02 -0800

    Added Backend for adding custom recipes.

--- source/assets/scripts/recipe-add.js
+++ source/assets/scripts/recipe-add.js
@@ -3,6 +3,8 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+import * as backend from './backend.js';
+
 var ingredientIndex = 1;
 var instructionIndex = 1;
 
@@ -17,9 +19,99 @@ async function init() {
   goSearch();
   goAdd();
   goSettings();
+  
+
+  addNewRecipe();
 
 }
 
+function addNewRecipe() {
+  const form = document.getElementById('add-recipe-form');
+
+  form.addEventListener('submit', (event) => {
+      // handle the form data
+      console.log("New Recipe Added");
+
+      event.preventDefault();
+      let recipe = {};
+
+      const nameField = document.getElementById('recipeName').value;
+      recipe.name = nameField;
+
+      const cookingTimeField = document.getElementById('cookingTime').value;
+      recipe.readyInMinutes = cookingTimeField;
+
+      const servingSizeField = document.getElementById('servingSize').value;
+      recipe.servings = servingSizeField;
+
+      let radios = document.getElementsByName('diff');
+      for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          // do whatever you want with the checked radio
+          recipe.difficulty = radios[i].value;
+          console.log(radios[i].value);
+          // only one radio can be logically checked, don't check the rest
+          break;
+        }
+      }
+
+      var veganBox = document.getElementById("vegan");
+      if (veganBox.checked == true){
+        recipe.vegan = true;
+      } else{
+        recipe.vegan = false;
+      }
+
+      var veggieBox = document.getElementById("veggie");
+      if (veggieBox.checked == true){
+        recipe.vegetarian = true;
+      } else{
+        recipe.vegetarian = false;
+      }
+
+      //add image ???
+      
+      let ingredientListLength = document.querySelectorAll("#ingredientOrderedList li").length;;
+
+      let ingredientArr = [];
+      console.log(ingredientListLength);
+
+      for(let i = 1; i <= ingredientListLength; i++){
+        let str = "ingredient-"+i;
+        console.log(str);
+        let ing = document.getElementById(str);
+        console.log(ing.value);
+        let theIngredient = {original:ing.value};
+        ingredientArr[i-1] = theIngredient;
+      }
+      recipe.ingredients = ingredientArr;
+
+      //instruction 
+      let instructionListLength = document.querySelectorAll("#instructionOrderedList li").length;;
+
+      let instructionArr = [];
+      console.log(instructionListLength);
+
+      for(let i = 1; i <= instructionListLength; i++){
+        let str = "instruction-"+i;
+        console.log(str);
+        let ing = document.getElementById(str);
+        console.log(ing.value);
+        let step = {number:i, step:ing.value};
+        instructionArr[i-1] = step;
+      }
+      recipe.steps = instructionArr;
+
+      console.log(recipe);
+
+      backend.add_recipe(recipe, true);
+
+      window.location.replace('index.html');
+
+  });
+  
+
+}
 /**
  * Click to go back to dashboard
  */
@@ -76,10 +168,11 @@ function addIngredient() {
     console.log('inside');
     ingredientIndex += 1;
     let node = document.createElement('LI');  
-    node.id = `ingredient-${ingredientIndex}`;
+    node.id = `ingredientNode-${ingredientIndex}`;
     let nodeInput = document.createElement('input');
     let br = document.createElement('br');
     nodeInput.type='text';
+    nodeInput.id = `ingredient-${ingredientIndex}`;
     nodeInput.appendChild(br);
     node.appendChild(nodeInput);
     let img = document.createElement('img');
@@ -104,10 +197,11 @@ function addInstruction() {
     console.log('inside');
     instructionIndex += 1;
     let node = document.createElement('LI');  
-    node.id = `instruction-${instructionIndex}`;
+    node.id = `instructionNode-${instructionIndex}`;
     let nodeInput = document.createElement('input');
     let br = document.createElement('br');
     nodeInput.type='text';
+    nodeInput.id = `instruction-${instructionIndex}`;
     nodeInput.appendChild(br);
     node.appendChild(nodeInput);
     let img = document.createElement('img');
