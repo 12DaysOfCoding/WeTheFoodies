7311d38b6e6fd1af7c74d3eb3bce2b49c2bcd4db
Harry Lei <zil026@ucsd.edu>
2021-12-01 20:59:29 -0800

    Merge branch 'main' of https://github.com/cse110-fa21-group30/cse110-fa21-group30

--- tests/e2e/settings.test.js
+++ tests/e2e/settings.test.js
@@ -17,7 +17,9 @@ describe('preference-setting test', () => {
     expect(text2['_remoteObject'].value).toBe("SAVED");
   });
 });
-
+/**
+ * Check search filter
+ */
 describe('recipe-searchPage load test', () => {
   beforeAll(async () => {
     await page.goto('http://127.0.0.1:5500/source/recipe-searchPage.html');
@@ -33,5 +35,41 @@ describe('recipe-searchPage load test', () => {
     let text_check = await page.$('.nav-search #nav-dashboard-text');
     let text = await text_check.getProperty('innerText');
     expect(text['_remoteObject'].value).toBe("SEARCH");
+  });
+});
+
+/**
+ * check version page
+ */
+describe('check version page', () => {
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500/source/version.html');
+  });
+
+  it('check version page', async () => {
+    let version = await page.$("#v-test");
+    let text = await version.getProperty('innerText');
+    expect(text['_remoteObject'].value).toBe("Version beta 1.0");
+  });
+});
+
+
+/**
+ * go to setting page from dashboard
+ */
+describe('go to setting', () => { 
+  beforeAll(async () => {
+    await page.goto('http://127.0.0.1:5500/source/index.html');
+  });
+
+  it('go to setting page from dashboard', async () => {
+    let settings = await page.$('.nav-settings');
+    await Promise.all([
+      settings.click(),
+      page.waitForNavigation(),
+    ]);
+    let font = await page.$('#setting-text');
+    let text = await font.getProperty('innerText');
+    expect(text['_remoteObject'].value).toBe("FONTSIZE");
   });
 });
