7a2009249da07c4d2c9a65b133d762f2670eff72
Yizhou Wang <81718264+yiw008@users.noreply.github.com>
2021-12-03 13:30:41 -0800

    Update recipe-detail.js

--- source/assets/scripts/recipe-detail.js
+++ source/assets/scripts/recipe-detail.js
@@ -9,12 +9,6 @@ import * as backend from './backend.js';
  * Initialize and call other function
  */
 async function init() {
-  
-  console.log(document.referrer);
-  
-  let index = document.referrer.lastIndexOf('/');
-  let str = document.referrer.substring(index + 1);
-  console.log(str);
 
   const recipe = backend.get_recipe(backend.get_selected());
 
@@ -88,8 +82,14 @@ function bindFoodieButton() {
 
 function goBack(){
   const btn = document.getElementById('dount');
+  let index = document.referrer.lastIndexOf('/');
+  let str = document.referrer.substring(index + 1);
+  console.log(str);
 
   btn.addEventListener('click', () => {
     window.history.back();
+    if (str === 'index.html'){
+      window.location.reload();  
+    }
   });
 }
