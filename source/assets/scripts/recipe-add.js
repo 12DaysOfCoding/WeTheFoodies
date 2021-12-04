99fe9d83cee103b99522c18fbb87540ae4c89285
Lexseal Lin <zhongjiezhex12@me.com>
2021-12-04 11:50:12 -0800

    fix add recipe to go with this change

--- source/assets/scripts/recipe-add.js
+++ source/assets/scripts/recipe-add.js
@@ -79,7 +79,7 @@ function addNewRecipe() {
       console.log(str);
       let ing = document.getElementById(str);
       // check whether the li is deleted - if deleted, it is null, don't add.
-      if (ing !== null){
+      if (ing && ing.value){
         console.log(ing.value);
         let theIngredient = {original:ing.value};
         ingredientArr[ingredientArrIndex] = theIngredient;
@@ -101,7 +101,7 @@ function addNewRecipe() {
       console.log(str);
       let ing = document.getElementById(str);
       // check whether the li is deleted - if deleted, it is null, don't add.
-      if (ing !== null){
+      if (ing && ing.value){
         console.log(ing.value);
         let step = {number:i, step:ing.value};
         instructionArr[instructionArrIndex] = step;
