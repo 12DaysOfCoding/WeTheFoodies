c5f4d8fa33e7dea561ee25fab376315678e31eea
Harry Lei <zil026@ucsd.edu>
2021-12-04 19:38:15 -0800

    create preference setting E2E tests

---
+++ tests/e2e/preference-setting.test.js
@@ -0,0 +1,115 @@
+describe('preference-setting test', () => {
+    beforeAll(async () => {
+      await page.goto('http://cse110-group30-affd4.web.app/preference-setting.html');
+    });
+  
+    /**
+       * Check save button
+       */
+    it('responds to "save" button press', async () => {
+      let saved_check = await page.$('#save-or-not');
+      let text = await saved_check.getProperty('innerText');
+      expect(text['_remoteObject'].value).toBe("SAVE");
+  
+      let button = await page.$('button');
+      await button.click();
+      let text2 = await saved_check.getProperty('innerText');
+      expect(text2['_remoteObject'].value).toBe("SAVED");
+    });
+  });
+  
+  describe('preference-setting test', () => {
+    beforeAll(async () => {
+      await page.goto('http://cse110-group30-affd4.web.app/preference-setting.html');
+    });
+  
+    /**
+       * Check save button after reload
+       */
+    it('responds to "save" button press', async () => {
+      let saved_check = await page.$('#save-or-not');
+      let text = await saved_check.getProperty('innerText');
+      expect(text['_remoteObject'].value).toBe("SAVE");
+  
+      let button = await page.$('button');
+      await button.click();
+      let text2 = await saved_check.getProperty('innerText');
+      expect(text2['_remoteObject'].value).toBe("SAVED");
+  
+      await page.reload();
+      let new_check = await page.$('#save-or-not');
+      let new_text = await new_check.getProperty('innerText');
+      expect(new_text['_remoteObject'].value).toBe("SAVE");
+    });
+  
+    /**
+     * Saving preferences as dairy-free and gluten-free 
+     * shows up in add recipe page, search page, and settings page
+     */
+    it('setting preferences as dairy-free and gluten-free saves correctly', async () => {
+      /**
+       * Set preference from preference setting page
+       */
+      let checkboxes = await page.$$('label');
+      await checkboxes[2].click();
+      await checkboxes[4].click();
+      let saved_check = await page.$('#save-or-not');
+      let button = await page.$('button');
+      await button.click();
+      let text2 = await saved_check.getProperty('innerText');
+      expect(text2['_remoteObject'].value).toBe("SAVED");
+      
+      /**
+       * check preference from recipe add page
+       */
+      await page.goto('https://cse110-group30-affd4.web.app/recipe-add.html');
+      let addRecipeCheckboxes = await page.$$('.diet-restrictionBox input');
+      let checkedCorrectly = true;
+      for (let i = 0; i < addRecipeCheckboxes.length ; i++) {
+        let checked = await (await addRecipeCheckboxes[i].getProperty('checked')).jsonValue();
+        if (i == 2 || i == 4) {
+          if (! checked) { checkedCorrectly = false; }
+        }
+        else {
+          if (checked) { checkedCorrectly = false; }
+        }
+      }
+  
+      /**
+       * check preference from recipe search page
+       */
+      await page.goto('https://cse110-group30-affd4.web.app/recipe-search.html');
+      let searchCheckboxes = await page.$$('.diet-restrictionBox input');
+      for (let i = 0; i < searchCheckboxes.length ; i++) {
+        let checked = await (await searchCheckboxes[i].getProperty('checked')).jsonValue();
+        if (i == 2 || i == 4) {
+          if (! checked) { checkedCorrectly = false; }
+        }
+        else {
+          if (checked) { checkedCorrectly = false; }
+        }
+      }
+  
+      /**
+       * check preference from preference setting page
+       */
+      await page.goto('https://cse110-group30-affd4.web.app/preference-setting.html');
+      let settingsCheckboxes = await page.$$('input');
+      for (let i = 0; i < settingsCheckboxes.length ; i++) {
+        let checked = await (await settingsCheckboxes[i].getProperty('checked')).jsonValue();
+        if (i == 2 || i == 4) {
+          if (! checked) { checkedCorrectly = false; }
+        }
+        else {
+          if (checked) { checkedCorrectly = false; }
+        }
+      }
+      expect(checkedCorrectly).toBe(true);
+    });
+  });
+
+//To Do: set preference from left div and check
+//To Do: set preference from right div and check
+//To Do: set preference to nothing and check
+//To Do: set preference and reload before save
+//To Do: set preference and reload after save
