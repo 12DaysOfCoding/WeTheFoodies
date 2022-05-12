0fa73889272bd278c71a76a96a7cddcb1d3ae61d
bghoang <bghoang@ucsd.edu>
2022-05-11 21:26:35 -0700

    working on addRecipe function for backend

---
+++ source/assets/scripts/api.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVqGsU9giK0GDQI7OOchNseJzMtn7uDuc",
+  authDomain: "wethefoodies-329cf.firebaseapp.com",
+  projectId: "wethefoodies-329cf",
+  storageBucket: "wethefoodies-329cf.appspot.com",
+  messagingSenderId: "560754808557",
+  appId: "1:560754808557:web:c6922900dad39cfafa1ee4",
+  measurementId: "G-5J4GYWY5VS",
+};
+
+export const app = initializeApp(firebaseConfig);
+
+//export app;
---
+++ source/assets/scripts/request.js
@@ -0,0 +1,120 @@
+import { app } from "./api.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  child,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.8.0/firebase-database.js";
+
+const db = getDatabase(app);
+
+/*function addRecipe(userId) {
+  set(ref(db, "users/" + userId), {
+    test: "test",
+  });
+}*/
+
+function getRecipe(userId) {
+  const dbRef = ref(db);
+  let finalData = null;
+  get(child(dbRef, `users/${userId}`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+        finalData = snapshot.val();
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return finalData;
+}
+
+function getRecipeWithValue(userId, value) {
+  const dbRef = ref(db);
+  let finalData = [];
+  //const userId = auth.currentUser.uid;
+  get(child(dbRef, `users/${userId}/UserRecipies`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot) => {
+          if (childSnapshot.val().recipeName === value) {
+            finalData.push(childSnapshot.val());
+          }
+        });
+        //console.log(snapshot.val());
+        //console.log(dataReturn);
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  return finalData;
+}
+
+let check = getRecipe("123");
+console.log(check);
+
+// Working on adding recipe,
+// 2 different types of add
+// First type: when user search for a recipe and save to favortie, on backend we save hash from third party api to the database.
+// Second type: add recipe normally to the database.
+
+/*
+UserId: String
+FavoriteRecipes 
+    NameOfRecipe: String (Use name search call)
+UserRecipes
+    NameOfRecipe: String
+        Serving: int
+        CookTime: int
+        Instructions
+            Instruction: String
+            ...
+        DietRestrictions
+            RestrictionStatus: boolean
+            ... requires client side
+            model to be in agreement
+*/
+
+// Add recipe to UserRecipes list
+function addRecipe(
+  userId,
+  recipeName,
+  Serving,
+  cookTime,
+  Instructions,
+  dietRestrictions
+) {
+  const currRecipe = ref(db, "users/" + userId + "/UserRecipies");
+  const newRecipe = push(currRecipe);
+  set(newRecipe, {
+    recipeName: recipeName,
+    Serving: Serving,
+    CookTime: cookTime,
+    Instructions: Instructions,
+    DietRestrictions: dietRestrictions,
+  });
+}
+
+/*addRecipe("124", "test", 2, 1, ["test1", "test2"], {
+  vegan: true,
+  glutenFree: false,
+});*/
+
+function addFavorite(userId, hashKey) {
+  const currRecipe = ref(db, "users/" + userId + "/FavoriteRecipes");
+  const newRecipe = push(currRecipe);
+  set(newRecipe, {
+    hashKey: hashKey,
+  });
+}
+
+//addFavorite("123", "dafkjafk");
