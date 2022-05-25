69ff003ba8a24bf4a85936d544209d255c6ba530
bghoang <bghoang@ucsd.edu>
2022-05-24 19:35:02 -0700

    use local storage to only let the tutorial run once

--- source/assets/scripts/tutorial-homepage.js
+++ source/assets/scripts/tutorial-homepage.js
@@ -1,51 +1,54 @@
-// add a flag when we're done
-/*introJs().oncomplete(function() {
-  localStorage.setItem('doneTour', 'yeah!');
-})
+const tutorial = introJs();
 
-// add a flag when we exit
-introJs().onexit(function() {
-   localStorage.setItem('doneTour', 'yeah!');
+// and check for it when deciding whether to start.
+window.addEventListener("load", function () {
+  var doneTour = localStorage.getItem("doneTour") === "yeah!";
+  if (doneTour) return;
+  else {
+    tutorial
+      .setOptions({
+        tooltipClass: "customTooltip",
+        steps: [
+          {
+            title: "Welcome",
+            intro:
+              "Welcome to We The Foodies, where you can find or create all the recipes that you want. 👋",
+          },
+          {
+            element: document.querySelector(".nav-bar"),
+            intro: "This is the navigation bar. There are 4 options available",
+            position: "top",
+          },
+          {
+            element: document.querySelector(".nav-dashboard"),
+            intro:
+              "This leads to the front page, where it display all the current recipes",
+          },
+          {
+            element: document.querySelector(".nav-search"),
+            intro: "This is where you search for any recipes that you want",
+          },
+          {
+            element: document.querySelector(".nav-add"),
+            intro: "This is where you add your own recipe",
+          },
+          {
+            element: document.querySelector(".nav-settings"),
+            intro: "This is where you can change the setting",
+          },
+        ],
+      })
+      .start();
+  }
 });
 
-// and check for it when deciding whether to start. 
-window.addEventListener('load', function() {
-  var doneTour = localStorage.getItem('doneTour') === 'yeah!';
-  if (doneTour) return;
-  introJS().start();
-}*/
+// add a flag when we're done
+tutorial.oncomplete(function () {
+  console.log("done tutorial");
+  localStorage.setItem("doneTour", "yeah!");
+});
 
-introJs()
-  .setOptions({
-    tooltipClass: "customTooltip",
-    steps: [
-      {
-        title: "Welcome",
-        intro:
-          "Welcome to We The Foodies, where you can find or create all the recipes that you want. 👋",
-      },
-      {
-        element: document.querySelector(".nav-bar"),
-        intro: "This is the navigation bar. There are 4 options available",
-        position: "top",
-      },
-      {
-        element: document.querySelector(".nav-dashboard"),
-        intro:
-          "This leads to the front page, where it display all the current recipes",
-      },
-      {
-        element: document.querySelector(".nav-search"),
-        intro: "This is where you search for any recipes that you want",
-      },
-      {
-        element: document.querySelector(".nav-add"),
-        intro: "This is where you add your own recipe",
-      },
-      {
-        element: document.querySelector(".nav-settings"),
-        intro: "This is where you can change the setting",
-      },
-    ],
-  })
-  .start();
+// add a flag when we exit
+tutorial.onexit(function () {
+  localStorage.setItem("doneTour", "yeah!");
+});
