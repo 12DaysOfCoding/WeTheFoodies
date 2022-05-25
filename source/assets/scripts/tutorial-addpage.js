4278bfc75a7cb812ede0b61f0d2ddb10fc5af884
bghoang <bghoang@ucsd.edu>
2022-05-24 21:44:27 -0700

    making sure all the tutorial only run once

--- source/assets/scripts/tutorial-addpage.js
+++ source/assets/scripts/tutorial-addpage.js
@@ -1,27 +1,46 @@
-introJs()
-  .setOptions({
-    steps: [
-      {
-        title: "Welcome",
-        intro: "This is the place where you add your own recipe",
-      },
-      {
-        element: document.querySelector(".basic"),
-        intro: "Type in all the necessary information here",
-        position: "top",
-      },
-      {
-        element: document.querySelector(".radioClass"),
-        intro: "Select the option you want",
-      },
-      {
-        element: document.querySelector(".diet-restrictionBox"),
-        intro: "Select the diet options you want",
-      },
-      {
-        element: document.querySelector(".bottom-boxes"),
-        intro: "This is where you add your own recipe",
-      },
-    ],
-  })
-  .start();
+const tutorialAddPage = introJs();
+
+// and check for it when deciding whether to start.
+window.addEventListener("load", function () {
+  var doneTour = localStorage.getItem("doneTourAddPage") === "yeah!";
+  if (doneTour) return;
+  else {
+    tutorialAddPage
+      .setOptions({
+        steps: [
+          {
+            title: "Welcome",
+            intro: "This is the place where you add your own recipe",
+          },
+          {
+            element: document.querySelector(".basic"),
+            intro: "Type in all the necessary information here",
+            position: "top",
+          },
+          {
+            element: document.querySelector(".radioClass"),
+            intro: "Select the option you want",
+          },
+          {
+            element: document.querySelector(".diet-restrictionBox"),
+            intro: "Select the diet options you want",
+          },
+          {
+            element: document.querySelector(".bottom-boxes"),
+            intro: "This is where you add your own recipe",
+          },
+        ],
+      })
+      .start();
+  }
+});
+
+// add a flag when we're done
+tutorialAddPage.oncomplete(function () {
+  localStorage.setItem("doneTourAddPage", "yeah!");
+});
+
+// add a flag when we exit
+tutorialAddPage.onexit(function () {
+  localStorage.setItem("doneTourAddPage", "yeah!");
+});
