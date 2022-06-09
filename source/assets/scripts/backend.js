6fdc0049f20dd64ad4292f4fb50f977552252e5c
Peter Gonzalez <p8gonzal@ucsd.edu>
2022-06-08 17:31:56 -0700

    New key

--- source/assets/scripts/backend.js
+++ source/assets/scripts/backend.js
@@ -2,7 +2,7 @@
 
 import { recipe_data, keep_fields } from './recipe-data.js';
 
-const API_KEY = '486eca841c6a49b896486723439f9977';
+const API_KEY = 'cd5607bd33744aa3a4b30b8d87ffb894';
 const CUSTOM_RECIPE_KEY = '%custom_recipes';
 const FAVORITE_RECIPE_KEY = '%favorite_recipes';
 const SELECTED_RECIPE_KEY = '%selected_recipe';
